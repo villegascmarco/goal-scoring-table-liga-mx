ad02b8bec90ffebf7f6038ae3d78674ec5d82bff
Villegas <villegascmarco@gmail.com>
2021-07-10 23:16:14 -0500

    Principal main.py is now working

---
+++ main.py
@@ -0,0 +1,43 @@
+import logging
+import subprocess
+import os
+import glob
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    extract()
+    transform()
+    load()
+
+
+def extract():
+
+    subprocess.run(['python', 'main.py'], cwd='./download')
+
+    subprocess.run(['move', r'download\*.csv', r'transform'], shell=True)
+
+
+def transform():
+
+    for file in glob.glob('transform/*.csv'):
+        subprocess.run(
+            ['python', 'main.py', file], cwd='./transform')
+        os.remove(file)
+        subprocess.run(['move', r'transform\*.csv', r'upload'],
+                       shell=True)
+
+
+def load():
+
+    for file in glob.glob('upload/*.csv'):
+        file = file.replace('upload\\', '')
+        subprocess.run(
+            ['python', 'main.py', file], cwd='./upload')
+        os.remove(f'upload/{file}')
+
+
+if __name__ == '__main__':
+    main()
